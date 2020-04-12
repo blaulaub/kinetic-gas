155a2e36a6bd0ac561e536a6ea0b2224e7f57e3b
Christian Fuchs <cfuchs@patchcode.ch>
2020-04-12 14:20:58 +0200

    time marching, fixed energy conversion

--- src/crash.cpp
+++ src/crash.cpp
@@ -83,12 +83,12 @@ struct Wall
 vector<Wall> originCubicWalls(double extend)
 {
   return {
-    { { extend, 0., 0.}, 0. },
-    { {-extend, 0., 0.}, extend },
-    { { 0., extend, 0.}, 0. },
-    { { 0.,-extend, 0.}, extend },
-    { { 0., 0., extend}, 0. },
-    { { 0., 0.,-extend}, extend }
+    { { 1., 0., 0.},     0. },
+    { {-1., 0., 0.}, extend },
+    { { 0., 1., 0.},     0. },
+    { { 0.,-1., 0.}, extend },
+    { { 0., 0., 1.},     0. },
+    { { 0., 0.,-1.}, extend }
   };
 }
 
@@ -127,13 +127,14 @@ optional<WallParticleCollision> ofNext(Wall &wall1, Particle &part1)
   double w3 = wall1.norm[2];
   double wo = wall1.offset;
   double k1 = w1*v1 + w2*v2 + w3*v3;
+  if (k1 >= 0) return nullopt;
   double k2 = w1*x1 + w2*x2 + w3*x3 + wo - r;
   double k3 = -k2/k1;
-  if (k3 < 0) return nullopt;
+  if (k3 <= 0) return nullopt;
   return WallParticleCollision(k3, wall1, part1);
 }
 
-optional<WallParticleCollision> ofNext(vector<Wall> &walls, vector<Particle> particles)
+optional<WallParticleCollision> ofNext(vector<Wall> &walls, vector<Particle> &particles)
 {
   tuple<bool, double, Wall*, Particle*> next = make_tuple(false, 0, nullptr, nullptr);
   for(int i = 0; i < particles.size(); i++)
@@ -175,15 +176,16 @@ optional<ParticleParticleCollision> ofNext(Particle &part1, Particle &part2)
   double v2 = part1.velocity[1] - part2.velocity[1];
   double v3 = part1.velocity[2] - part2.velocity[2];
   double k1 = v1*x1 + v2*x2 + v3*x3;
+  if (k1 >= 0) return nullopt;
   double k2 = v1*v1 + v2*v2 + v3*v3;
   double k3 = x1*x1 + x2*x2 + x3*x3 - 4*r*r;
   double k4 = 1./k2;
   double k5 = -k1*k4;
   double k6 = -k3*k4;
   double k7 = (k5*k5 + k6);
-  if (k7 < 0) return nullopt;
+  if (k7 <= 0) return nullopt;
   double k8 = k5 - sqrt(k7);
-  if (k8 < 0) return nullopt;
+  if (k8 <= 0) return nullopt;
   return ParticleParticleCollision(k8, part1, part2);
 }
 
@@ -209,7 +211,7 @@ optional<ParticleParticleCollision> ofNext(vector<Particle> &particles)
   return nullopt;
 }
 
-void collide(Wall wall1, Particle part1)
+void collide(Wall &wall1, Particle &part1)
 {
   double v1 = part1.velocity[0];
   double v2 = part1.velocity[1];
@@ -223,10 +225,9 @@ void collide(Wall wall1, Particle part1)
     v2 - 2*k1*w2,
     v3 - 2*k1*w3
   };
-  // TODO update next collisions for part1
 }
 
-void collide(Particle part1, Particle part2)
+void collide(Particle &part1, Particle &part2)
 {
   double v11 = part1.velocity[0];
   double v12 = part1.velocity[1];
@@ -242,9 +243,13 @@ void collide(Particle part1, Particle part2)
   double w1 = x1*k2;
   double w2 = x2*k2;
   double w3 = x3*k2;
-  double k4 = w1*(v11-v21);
-  double k5 = w2*(v12-v22);
-  double k6 = w3*(v13-v23);
+  double v1 = v11 - v21;
+  double v2 = v12 - v22;
+  double v3 = v13 - v23;
+  double k3 = v1*w1 + v2*w2 + v3*w3;
+  double k4 = w1*k3;
+  double k5 = w2*k3;
+  double k6 = w3*k3;
   // TODO the signs could be wrong
   part1.velocity = {
     v11 - k4,
@@ -252,12 +257,10 @@ void collide(Particle part1, Particle part2)
     v13 - k6
   };
   part2.velocity = {
-    v21 - k4,
-    v22 - k5,
-    v23 - k6
+    v21 + k4,
+    v22 + k5,
+    v23 + k6
   };
-  // TODO update next collisions for part1
-  // TODO update next collisions for part2
 }
 
 int main(int argc, char** args)
@@ -270,18 +273,45 @@ int main(int argc, char** args)
 
   vector<Particle> particles = particlesWithinOriginCubicle(1., 100);
 
-  // init wall collisions
-  auto nextWallParticleCollision = ofNext(walls, particles);
-  if (nextWallParticleCollision)
+  double time = 0.;
+  while (time < 10.)
   {
-    cout << "next wall collision in " << nextWallParticleCollision.value().time << " sec." << endl;
-  }
+    auto nextWallParticleCollision = ofNext(walls, particles);
+    auto nextParticleCollision = ofNext(particles);
+    // TODO computing next collision each time over is a) inefficient and b) could be inaccurate
 
-  // init particle collisions
-  auto nextParticleCollision = ofNext(particles);
-  if (nextParticleCollision)
-  {
-    cout << "next particle collision in " << nextParticleCollision.value().time << " sec." << endl;
+    double deltaT;
+    bool nextIsParticleCollision = nextParticleCollision && nextParticleCollision.value().time < nextWallParticleCollision.value().time;
+    if (nextIsParticleCollision)
+    {
+      cout << "next particle collision in " << nextParticleCollision.value().time << " sec." << endl;
+      deltaT = nextParticleCollision.value().time;
+    }
+    else
+    {
+      cout << "next wall collision in " << nextWallParticleCollision.value().time << " sec." << endl;
+      deltaT = nextWallParticleCollision.value().time;
+    }
+
+    // advance particles
+    for(int i = 0; i < particles.size(); i++)
+    {
+      particles[i].position[0] += particles[i].velocity[0] * deltaT;
+      particles[i].position[1] += particles[i].velocity[1] * deltaT;
+      particles[i].position[2] += particles[i].velocity[2] * deltaT;
+    }
+    time += deltaT;
+
+    // update collision
+    if (nextIsParticleCollision)
+    {
+      collide(nextParticleCollision.value().particle1, nextParticleCollision.value().particle2);
+    }
+    else
+    {
+      collide(nextWallParticleCollision.value().wall, nextWallParticleCollision.value().particle);
+    }
+
   }
 
   return 0;
