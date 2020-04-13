360984ce815592bd83bc8dca4b726475da99233d
Christian Fuchs <cfuchs@patchcode.ch>
2020-04-13 11:45:12 +0200

    split video impl into source file

--- src/video_renderer.h
+++ src/video_renderer.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <stdio.h>
 #include <string>
+
+class AVCodec;
+class AVCodecContext;
+class AVFrame;
+class AVPacket;
 
 class VideoRenderer
 {
@@ -18,68 +24,9 @@ private:
 
 public:
 
-  VideoRenderer(const std::string& filename, int width, int height, int fps)
-  : _filename(filename), _width(width), _height(height)
-  {
+  VideoRenderer(const std::string& filename, int width, int height, int fps);
 
-    codec = avcodec_find_encoder(AV_CODEC_ID_MPEG2VIDEO);
-    if (!codec) {
-      fprintf(stderr, "codec not found\n");
-      exit(1);
-    }
+  ~VideoRenderer();
 
-    c = avcodec_alloc_context3(codec);
-
-    c->bit_rate = 400000;
-    c->width = width;
-    c->height = height;
-    c->time_base = (AVRational){1,fps};
-    c->framerate = (AVRational){fps,1};
-    c->gop_size = 10; /* emit one intra frame every ten frames */
-    c->max_b_frames=1;
-    c->pix_fmt = AV_PIX_FMT_YUV420P;
-
-    if (avcodec_open2(c, codec, NULL) < 0) {
-      fprintf(stderr, "could not open codec\n");
-      exit(1);
-    }
-
-    f = fopen("crash.mpg", "wb");
-    if (!f) {
-      fprintf(stderr, "could not open %s\n", "crash.mpg");
-      exit(1);
-    }
-
-    avpkt = av_packet_alloc();
-    if (!avpkt) {
-      fprintf(stderr, "could not alloc pkt\n");
-      exit(1);
-    }
-
-  }
-
-  ~VideoRenderer()
-  {
-    encode(NULL);
-    fclose(f);
-
-    avcodec_free_context(&c);
-    av_packet_free(&avpkt);
-  }
-
-  void encode(AVFrame *yuvpic)
-  {
-    int ret = avcodec_send_frame(c, yuvpic);
-    while (ret >= 0)
-    {
-      ret = avcodec_receive_packet(c, avpkt);
-      if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) break;
-      if (ret < 0) {
-        fprintf(stderr, "error while encoding\n");
-        exit(1);
-      }
-      fwrite(avpkt->data, 1, avpkt->size, f);
-      av_packet_unref(avpkt);
-    }
-  }
+  void encode(AVFrame *yuvpic);
 };
